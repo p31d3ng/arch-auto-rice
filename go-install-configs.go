c6cfa01f835720b2baca700720ee9329658b8a99
Pei Deng <p31.d3ng@gmail.com>
2019-03-03 13:25:20 -0800

    finished start script, hope it runs in one pass

---
+++ go-install-configs.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, this is a placeholder")
+}
---
+++ start.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# THIS SCRIPT NEED TO RUN UNDER ROOT AFTER CLEAN ARCH INSTALLATION
+# update arch
+pacmac -Syu --noconfirm
+pacman -S git --noconfirm
+pacman -S sudoer --noconfirm
+pacman -S yay --noconfirm
+
+# create sudo group
+read -p "Group for sudoers(default: super): " group_name
+group_name=${group_name:-super}
+super_group_exist=$(cut -d: -f1 /etc/group | grep "super" | wc -l)
+if [[ ${super_group_exist} -eq 0 ]]; then
+	echo "${group_name} does not exist, creating a new one..."
+	groupadd ${group_name}
+	sed -i '/%sudo.*/a # Allow members of group super to execute any comments\n%super  ALL=(ALL) NOPASSWD:ALL' /etc/sudoers
+else
+	echo "${group_name} exists! skipping creating group..."
+fi
+
+# create sudo user
+read -p "Username: " username
+user_exist=$(cut -d: -f1 /etc/passwd | grep "$username" | wc -l)
+if [[ ${user_exist} -gt 0 ]]; then
+	echo "User exist, adding to super group"
+	usermod -aG "${group_name}" "$username"
+else
+	echo "User does not exist, creating a new user..."
+	useradd -s /bin/bash -m -G "${group_name}" "$username"
+	# Setting password
+	echo "Setting password for $username"
+	passwd "$username"
+fi
+
+# install required packages
+while IFS='' read -r line || [[ -n "$line" ]]; do
+	echo "Installing $line"
+	pacman -S $line --noconfirm
+done < ./required-packages
+
+# install config files via go script
+go run go-install-configs.go
