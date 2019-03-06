3d3fd5031ee6c8d09862f9fc09c84d68def063dc
Pei Deng <pdeng@vmware.com>
2019-03-06 15:22:10 -0800

    fix omf

--- tasks/install_fish_deps.fish
+++ tasks/install_fish_deps.fish
@@ -1,8 +1,5 @@
 #!/usr/bin/env fish
 
-curl -L https://get.oh-my.fish > /tmp/omf-install
-fish /tmp/omf-install --noninteractive
-sleep 5
 for line in (cat $argv);
     omf install $line
 end
---
+++ tasks/install_omf.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+
+curl -L https://get.oh-my.fish > /tmp/omf-install
+fish /tmp/omf-install --noninteractive
+rm /tmp/omf-install
