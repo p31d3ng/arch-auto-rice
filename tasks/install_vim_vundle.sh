5532ad329c63f3feff97b303f3664e1a971b6e36
Pei Deng <p31.d3ng@gmail.com>
2019-03-07 01:39:52 -0800

    logical fix

--- tasks/install_vim_vundle.sh
+++ tasks/install_vim_vundle.sh
@@ -2,7 +2,6 @@
 
 set -e
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp $1 ~/.vimrc
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe && ./install.py
 cd -
