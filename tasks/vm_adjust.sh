321f5fd15718c327af1191b89512cad5ab6d932a
Pei Deng <pdeng@vmware.com>
2019-03-08 14:28:11 -0800

    add vm adjust

---
+++ tasks/vm_adjust.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if $(facter is_virtual); then
+    echo "You're running in VM, remove dpi overrides"
+    sed -i -e '/dpi/d' $1/Xresources
+    sed -i -e '/dpi/d' $1/rofi_config.rasi
+    sed -i -e '/dpi/d' $1/polybar_config
+    sed -i -e '1,1s/Mod4/Mod1/g' $1/i3_config
+    return
+fi
