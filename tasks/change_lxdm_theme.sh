143cd420a7ca7e45d615e8320517547990fb7fcb
Pei Deng <p31.d3ng@gmail.com>
2019-03-09 19:30:15 -0800

    bugfix

--- tasks/change_lxdm_theme.sh
+++ tasks/change_lxdm_theme.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo sed -i -e 's/^\(theme=\).*/\1Archlinux/' /etc/lxdm/lxdm.conf
+sudo sed -i -e 's/\# \(session=\).*/\1\/usr\/bin\/i3/' /etc/lxdm/lxdm.conf
--- tasks/set_utf8_locale.sh
+++ tasks/set_utf8_locale.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo sed -i 's/\#(en_US.UTF-8.*)/\1/' /etc/locale.gen
+sudo sed -i -e 's/\#(en_US.UTF-8.*)/\1/' /etc/locale.gen
 sudo locale-gen
 sudo bash -c "echo 'LANG=en_US.UTF-8' > /etc/locale.conf"
