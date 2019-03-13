9c53b9419cab1214cd6072c6eea3ae6b0bb83d52
Pei Deng <pdeng@vmware.com>
2019-03-13 14:45:14 -0700

    adding dejavu fonts and adding grub hidpi support

--- tasks/dpi_adjust.sh
+++ tasks/dpi_adjust.sh
@@ -7,6 +7,13 @@ fi
 
 if [[ $(xdpyinfo | grep dimensions | awk '/[0-9]+x[0-9]+/{print $2}') = "3840x2160" ]]; then
     echo "You're using 4K screen, keeping dpi as 220"
+    # -----------------------------------
+    # Adjusting grub font size
+    # making a font with size 36 for grub, then add it to grub config    
+    sudo grub-mkfont -s 36 -o /boot/grub/dejavu.pf2 /usr/share/fonts/TTF/DejaVuSansMono.ttf
+    sudo sed -i -e "\$aGRUB_FONT=/boot/grub/dejavu.pf2" /etc/default/grub
+    sudo grub-mkconfig -o /boot/grub/grub.cfg
+    # -----------------------------------
 else
     echo "You're using non-4K screen, remove dpi overrides"
     sed -i -e '/dpi/d' $1/Xresources
