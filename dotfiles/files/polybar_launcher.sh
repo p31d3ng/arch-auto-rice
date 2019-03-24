f64b658bb14752980a182686734310389b56c04e
Pei Deng <p31.d3ng@gmail.com>
2019-03-24 15:16:14 -0700

    update polybar

--- dotfiles/files/polybar_launcher.sh
+++ dotfiles/files/polybar_launcher.sh
@@ -2,6 +2,14 @@
 
 pkill polybar
 
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload i3wmthemer_bar &
+  done
+else
+  polybar --reload i3wmthemer_bar &
+fi
+
 sleep 1;
 
 polybar -r i3wmthemer_bar &
