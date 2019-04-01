a962c06a6ca2b238a49811d14fb520a063a69bd9
Pei Deng <p31.d3ng@gmail.com>
2019-03-31 19:28:08 -0700

    add emacs finally

---
+++ dotfiles/files/fish_emacs.fish
@@ -0,0 +1,4 @@
+alias emax="emacsclient -c" # open file in a *new* X-Window
+alias emat="emacsclient -t" # open file in the current terminal
+export VISUAL="emacsclient -c"
+export EDITOR="emacsclient -t"
