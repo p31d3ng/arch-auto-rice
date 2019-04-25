b5ec45cb8dfe039da4a23fed2e2e9a6aeeb39275
Pei Deng <pdeng@vmware.com>
2019-04-25 13:06:04 -0700

    use tee rather than cat

--- tasks/config_power_mgmt.sh
+++ tasks/config_power_mgmt.sh
@@ -2,7 +2,7 @@
 
 # Set power key to suspend-then-hibernate in case of fat finger, Closing lid should suspend (or go to hibernate later if on battery)
 # Also, after idling for 5min, the machine should suspend to save power.
-cat >> /etc/systemd/logind.conf << EOM
+sudo tee -a /etc/systemd/logind.conf > /dev/null << EOM
 HandlePowerKey=suspend-then-hibernate
 HandleLidSwitch=suspend-then-hibernate
 HandleLidSwitchExternalPower=suspend
@@ -11,8 +11,7 @@ IdelActionSec=5min
 EOM
 
 # After suspend for 300 seconds, the machine will go hibernation
-cat >> /etc/systemd/sleep.conf << EOM
-HibernateMode=platform shutdown
+sudo tee -a /etc/systemd/sleep.conf > /dev/null << EOM
 HibernateDelaySec=300
 EOM
 
