86e0adf515828b4b98f0aacb59f9f3d859420d7c
Pei Deng <p31.d3ng@gmail.com>
2019-04-25 07:46:34 -0700

    bugfix

--- tasks/enable_swap_hibernation.sh
+++ tasks/enable_swap_hibernation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 swap_partition=$(sudo fdisk -l | grep swap | awk '{print $1}')
-sudo sed -i -e "s|\(GRUB_CMDLINE_LINUX_DEAULT=\".*\)\(\"\)|\1${swap_partition}\2|" /etc/default/grub
+echo ${swap_partition}
+sudo sed -e "s|\(GRUB_CMDLINE_LINUX_DEFAULT=\".*\)\(\"\)|\1 resume=${swap_partition}\2|" /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
